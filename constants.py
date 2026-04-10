fc18b3e1e66d99d44891c381447c26e2700d7f82
Mahir <itsmahir24@gmail.com>
2026-04-10 21:59:05 +0200

    feat: Created constants for flags and colours

---
+++ constants.py
@@ -0,0 +1,37 @@
+FLAGS = {
+    "British":      "🇬🇧", "Dutch":        "🇳🇱", "Monegasque":   "🇲🇨",
+    "Spanish":      "🇪🇸", "Australian":   "🇦🇺", "Mexican":      "🇲🇽",
+    "Finnish":      "🇫🇮", "French":       "🇫🇷", "Canadian":     "🇨🇦",
+    "German":       "🇩🇪", "Thai":         "🇹🇭", "Japanese":     "🇯🇵",
+    "American":     "🇺🇸", "Chinese":      "🇨🇳", "Danish":       "🇩🇰",
+    "Italian":      "🇮🇹", "Argentine":    "🇦🇷", "Brazilian":    "🇧🇷",
+    "Austrian":     "🇦🇹", "Belgian":      "🇧🇪", "New Zealander":"🇳🇿",
+    "Swiss":        "🇨🇭", "Polish":       "🇵🇱", "Swedish":      "🇸🇪",
+}
+
+CONSTRUCTOR_COLOURS = {
+    "red_bull":         "#3671C6",
+    "mclaren":          "#FF8000",
+    "ferrari":          "#E8002D",
+    "mercedes":         "#27F4D2",
+    "aston_martin":     "#229971",
+    "alpine ":          "#FF87BC",
+    "haas":             "#B6BABD",
+    "rb":               "#6692FF",
+    "williams":         "#64C4FF",
+    "audi":             "#454A45",
+    "cadillac":         "#888C25"
+}
+
+NATIONAL_FLAGS = {
+    "United Kingdom": "🇬🇧", "Netherlands":    "🇳🇱", "Monaco":         "🇲🇨",
+    "Spain":          "🇪🇸", "Australia":      "🇦🇺", "Mexico":         "🇲🇽",
+    "Finland":        "🇫🇮", "France":         "🇫🇷", "Canada":         "🇨🇦",
+    "Germany":        "🇩🇪", "Thailand":       "🇹🇭", "Japan":          "🇯🇵",
+    "United States":  "🇺🇸", "China":          "🇨🇳", "Denmark":        "🇩🇰",
+    "Italy":          "🇮🇹", "Argentina":      "🇦🇷", "Brazil":         "🇧🇷",
+    "Austria":        "🇦🇹", "Belgium":        "🇧🇪", "New Zealand":    "🇳🇿",
+    "Switzerland":    "🇨🇭", "Poland":         "🇵🇱", "Sweden":         "🇸🇪",
+    "United Arab Emirates" : "", "Qatar": "", "Singapore": "",
+    "Azerbaijan": "", "Hungary": "",
+}
